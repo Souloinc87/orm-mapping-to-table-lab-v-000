a0bf70b15114bfd0926dd1a6faf4a5b082af9bd1
Marcus  = Soulo <souloinc87@gmail.com>
2018-09-18 22:17:28 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,12 +1,25 @@
 class Student
 
   attr_accessor :name, :grade
-  attr_reader :id 
+  attr_reader :id
 
   def initialize(name, grade, id = nil)
     @name = name
     @grade = grade
     @id = id
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+      id INTERGER PRIMARY KEY,
+      name TEXT,
+      grade INTERGER
+      )
+      SQL
+    DB[:conn].execute(sql)
+
+
   end
 
   # Remember, you can access your database connection anywhere in this class
