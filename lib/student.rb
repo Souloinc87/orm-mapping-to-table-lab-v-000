0455c99a141f70db4f347a9b1b356184bf2f95cc
Marcus  = Soulo <souloinc87@gmail.com>
2018-09-18 22:49:07 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,7 @@ class Student
   def self.create_table
       sql = <<-SQL
         CREATE TABLE IF NOT EXISTS students (
-        id INTERGER PRIMARY KEY,
+        id INTEGER PRIMARY KEY,
         name TEXT,
         grade INTEGER
         )
