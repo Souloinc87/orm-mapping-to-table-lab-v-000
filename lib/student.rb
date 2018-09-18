5a1c4bb79babecd40930ce99d7aca558cb65314b
Marcus  = Soulo <souloinc87@gmail.com>
2018-09-18 22:13:37 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,12 @@
 class Student
 
-  attr_accessor :name, :grade 
+  attr_accessor :name, :grade
+  attr_reader :id 
 
   def initialize(name, grade, id = nil)
     @name = name
     @grade = grade
-    @id = id 
+    @id = id
   end
 
   # Remember, you can access your database connection anywhere in this class
